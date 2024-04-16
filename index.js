ba8c207e6d2d41a8c55aa048668610a022a0cdfc
Paola <158229690+Xxpaus@users.noreply.github.com>
2024-04-16 18:18:33 -0500

    clase 17

---
+++ index.js
@@ -0,0 +1,72 @@
+/*SUSANA TIENE UNA VETERINARIA, QUIERE TENER DOS LISTAS DE ATENDIDOS Y NO ANTETIDOS
+TODAS LAS MASCOTAS TIENEN UN NOMBRE, UNA RAZA, UN NUMERO DE CONTACTO Y UN DUEÑO*/
+
+let listaAtendidos = []
+let listaNoAtendidos = []
+
+let dueño = {
+    nombre:" ",
+    apellido:" "
+}
+
+let mascota = {
+    nombre:"Rocky Ronaldo",
+    raza: "criollo",
+    numeroContacto: "300000123",
+    dueño:
+    { 
+        nombre:"Juan",
+        apellido:"Oquendo",
+    }
+}
+
+// Compare this snippet from clases/clase%15/index.js:
+
+function FactoryMascota(nombre, raza, numeroContacto, dueño) {
+    return {
+        nombre,
+        raza,
+        numeroContacto,
+        IsAtendidos,
+        dueño
+    }
+}
+
+function FactoryDueño(nombre, apellido) {
+    return {
+        nombre,
+        apellido
+    }
+}
+
+let mascota = FactoryMascota("Rocky Ronaldo", 
+"criollo",
+ "300000123",
+ true, 
+ FactoryDueño("Juan", "Oquendo"))
+ 
+let mascota1 = FactoryMascota("Firulais",
+    "Pitbull",
+    "123456",
+    false,
+    FactoryDueño("Miguel", "Alvarez"))
+
+function Veterinaria(mascota) {
+    let.listaAtendidos = []
+    let.listaNoAtendidos = []
+
+    if(mascota.IsAtendidos){
+        listaAtendidos.push(mascota)
+    } else{
+        listaNoAtendidos.push(mascota)
+    }
+
+        return{
+            listaAtendidos,
+            listaNoAtendidos
+        }
+}
+
+console.log(veterinaria(mascota).listaAtendidos)
+console.log(veterinaria(mascota1).listaAtendidos)
+// Path: clases/clase%2017/index.js
